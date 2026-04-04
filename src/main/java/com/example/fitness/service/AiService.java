b2864933f17bc72425216f9b75443976ec7beb49
Kshitij Kataria <kshitij.kataria.ug23@nsut.ac.in>
2026-04-04 11:47:45 +0530

    Integrated Gemini API for AI Powered Recommendations

---
+++ src/main/java/com/example/fitness/service/AiService.java
@@ -0,0 +1,54 @@
+package com.example.fitness.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class AiService {
+    @Value("AIzaSyCqdGpGmc2JAyQjvsPC1Um52hcTly_j9sA")
+    private String apiKey;
+
+    @Value("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent")
+    private String apiUrl;
+
+    private final WebClient webClient = WebClient.builder().build();
+
+    public String generateRecommendation(String prompt) {
+
+        Map<String, Object> request = Map.of(
+                "contents", List.of(
+                        Map.of(
+                                "parts", List.of(
+                                        Map.of("text", prompt)
+                                )
+                        )
+                )
+        );
+
+        return webClient.post()
+                .uri(apiUrl + "?key=" + apiKey)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .bodyValue(request)
+                .retrieve()
+                .bodyToMono(Map.class)
+                .map(response -> {
+                    List<Map<String, Object>> candidates =
+                            (List<Map<String, Object>>) response.get("candidates");
+
+                    Map<String, Object> content =
+                            (Map<String, Object>) candidates.get(0).get("content");
+
+                    List<Map<String, Object>> parts =
+                            (List<Map<String, Object>>) content.get("parts");
+
+                    return parts.get(0).get("text").toString();
+                })
+                .block();
+    }
+}
--- src/main/java/com/example/fitness/service/RecommendationService.java
+++ src/main/java/com/example/fitness/service/RecommendationService.java
@@ -7,9 +7,13 @@ import com.example.fitness.model.User;
 import com.example.fitness.repository.AcitivityRepository;
 import com.example.fitness.repository.RecommendationRepository;
 import com.example.fitness.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class RecommendationService {
@@ -17,11 +21,13 @@ public class RecommendationService {
     private final UserRepository userRepository;
     private final AcitivityRepository acitivityRepository;
     private final RecommendationRepository recommendationRepository;
+    private final AiService aiService;
 
-    public RecommendationService(UserRepository userRepository, AcitivityRepository acitivityRepository, RecommendationRepository recommendationRepository) {
+    public RecommendationService(UserRepository userRepository, AcitivityRepository acitivityRepository, RecommendationRepository recommendationRepository,  AiService aiService) {
         this.userRepository = userRepository;
         this.acitivityRepository = acitivityRepository;
         this.recommendationRepository = recommendationRepository;
+        this.aiService=aiService;
     }
 
     public Recommendation generateRecommendation(RecommendationRequest recommendationRequest) {
@@ -29,14 +35,78 @@ public class RecommendationService {
                 .orElseThrow(() ->new RuntimeException("user not found"+ recommendationRequest.getUserId()));
         Activity activity= acitivityRepository.findById(recommendationRequest.getActivityId())
                 .orElseThrow(() ->new RuntimeException("activity not found"+ recommendationRequest.getActivityId()));
-        Recommendation recommendation= Recommendation.builder()
-                .user(user)
-                .activity(activity)
-                .improvements(recommendationRequest.getImprovements())
-                .suggestions(recommendationRequest.getSuggestions())
-                .safety(recommendationRequest.getSafety())
-                .build();
-        return recommendationRepository.save(recommendation);
+        Map<String, Object> metrics = activity.getAdditionalMetrics();
+
+        Object sets = (metrics != null) ? metrics.get("sets") : "N/A";
+        Object reps = (metrics != null) ? metrics.get("reps") : "N/A";
+        List<String> existingExercises = new ArrayList<>();
+        if (metrics != null && metrics.get("exercises") != null) {
+            existingExercises = (List<String>) metrics.get("exercises");
+        }
+        String prompt = "Give fitness recommendations for this activity:\n" +
+                "Type: " + activity.getType() + "\n" +
+                "Duration: " + activity.getDuration() + " minutes\n" +
+                "Calories Burned: " + activity.getCaloriesBurned() + "\n" +
+                "Sets: " + sets + "\n" +
+                "Reps: " + reps + "\n" +
+                "Existing Exercises: " + existingExercises + "\n\n" +
+
+                "Also suggest top 3 NEW exercises excluding the above ones.\n\n" +
+
+                "Return strictly in JSON format like:\n" +
+                "{\n" +
+                "  \"improvements\": [\"\", \"\"],\n" +
+                "  \"suggestions\": [\"\", \"\"],\n" +
+                "  \"safety\": [\"\", \"\"],\n" +
+                "  \"newExercises\": [\"\", \"\", \"\"]\n" +
+                "}";
+
+        String aiResponse = aiService.generateRecommendation(prompt);
+
+        aiResponse = aiResponse.replace("```json", "")
+                .replace("```", "")
+                .trim();
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            aiResponse = aiResponse.substring(
+                    aiResponse.indexOf("{"),
+                    aiResponse.lastIndexOf("}") + 1
+            );
+
+
+            Map<String, List<String>> parsed = objectMapper.readValue(aiResponse, Map.class);
+
+            List<String> improvements = parsed.getOrDefault("improvements", List.of("No data"));
+            List<String> suggestions = parsed.getOrDefault("suggestions", List.of("No data"));
+            List<String> safety = parsed.getOrDefault("safety", List.of("No data"));
+            List<String> newExercises = parsed.getOrDefault("newExercises", List.of());
+
+            // ✅ Remove duplicates
+            List<String> finalExistingExercises = existingExercises;
+            newExercises = newExercises.stream()
+                    .filter(ex -> !finalExistingExercises.contains(ex))
+                    .limit(3)
+                    .toList();
+            String recommendationText = "Top 3 exercises you can try: " +
+                    String.join(", ", newExercises);
+
+
+            Recommendation recommendation = Recommendation.builder()
+                    .user(user)
+                    .activity(activity)
+                    .type(String.valueOf(activity.getType()))
+                    .recommendation(recommendationText)
+                    .improvements(improvements)
+                    .suggestions(suggestions)
+                    .safety(safety)
+                    .build();
+
+            return recommendationRepository.save(recommendation);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error parsing AI response: " + e.getMessage());
+        }
 
 
     }
